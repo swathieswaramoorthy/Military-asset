1f6b23d3b30b302931795926ac06995d573215ee
swathieswaramoorthy <swathieswaramoorthy2005@gmail.com>
2025-08-22 17:06:46 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
-// Import Routes
 import authRoutes from "./routes/authRoutes.js";
 import dashboardRoutes from "./routes/dashboard.js";
 import purchaseRoutes from "./routes/purchaseRoutes.js";
@@ -15,12 +14,10 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/purchases", purchaseRoutes);
@@ -28,20 +25,16 @@ app.use("/api/transfers", transferRoutes);
 app.use("/api/assignments", assignmentsRoutes);
 app.use("/api/expenditures", expenditureRoutes);
 
-// Default route
 app.get("/", (req, res) => {
   res.send("Military Asset Management System Backend is running...");
 });
 
-// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Server Error", error: err.message });
 });
 
-// MongoDB Atlas Connection
 const MONGO_URI = process.env.MONGO_URI;
-
 console.log("Connecting to MongoDB Atlas:", MONGO_URI);
 
 mongoose
